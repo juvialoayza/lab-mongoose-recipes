86170e472c46fdf128f6dda6572106d3b6d60e78
juvialoayza <113201289+juvialoayza@users.noreply.github.com>
2022-10-14 11:55:55 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = ('mongodb+srv://raniermujica:hannah1950@cluster0.ndc59ig.mongodb.net/?retryWrites=true&w=majority');
+const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -13,11 +13,45 @@ mongoose
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+
+    // ITERATION 2
+    // return Recipe.create({
+    //   title: "Asian Glazed Chicken Thighs",
+    //   level: "Amateur Chef",
+    //   ingredients: [
+    //     "1/2 cup rice vinegar",
+    //     "5 tablespoons honey",
+    //     "1/3 cup soy sauce (such as Silver Swan®)",
+    //     "1/4 cup Asian (toasted) sesame oil",
+    //     "3 tablespoons Asian chili garlic sauce",
+    //     "3 tablespoons minced garlic",
+    //     "salt to taste",
+    //     "8 skinless, boneless chicken thighs"
+    //   ],
+    //   cuisine: "Asian",
+    //   dishType: "main_course",
+    //   image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+    //   duration: 40,
+    //   creator: "Chef LePapu"
+    // })
+    
+    // ITERATION 3 
+    // return Recipe.insertMany(data)
+    
+    // ITERATION 4
+    // return Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100})
+
+    //ITERARION 5
+    // return Recipe.deleteOne({title: "Carrot Cake"})
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    // console.log("hemos borrado la Carrot Cake")
+    
+    // return Recipe.deleteMany()
   })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+  // mongoose.disconnect();
